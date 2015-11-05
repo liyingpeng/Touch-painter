a1ada5128924ff4a465f253912a58dd52b1a7e48
liyingpeng <liyingpeng@baidu.com>
2015-11-05 22:50:02 +0800

    initialized

---
+++ TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by liyingpeng on 15/11/5.
+//  Copyright (c) 2015年 liyingpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
