3927ae7de625629568cbebf2ae76a5bf5d281450
liyingpengc <liyingpeng@baidu.com>
2015-11-09 23:05:19 +0800

    add dot model

--- TouchPainter/Composite_Prototype_Iterator/Stroke.h
+++ TouchPainter/Composite_Prototype_Iterator/Stroke.h
@@ -7,7 +7,36 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Mark.h"
 
-@interface Stroke : NSObject
+@protocol MarkVisitor;
+
+@interface Stroke : NSObject <Mark>
+
+@property (nonatomic, strong) UIColor *color;
+@property (nonatomic, assign) CGFloat size;
+@property (nonatomic, assign) CGPoint location;
+@property (nonatomic, assign, readonly) NSUInteger count;
+@property (nonatomic, assign, readonly) id <Mark> lastChild;
+
+- (void) addMark:(id <Mark>) mark;
+- (void) removeMark:(id <Mark>) mark;
+- (id <Mark>) childMarkAtIndex:(NSUInteger) index;
+
+// for the Visitor pattern
+- (void) acceptMarkVisitor:(id <MarkVisitor>)visitor;
+
+// for the Prototype pattern
+- (id) copyWithZone:(NSZone *)zone;
+
+// for the Iterator pattern
+- (NSEnumerator *) enumerator;
+
+// for internal iterator implementation
+- (void) enumerateMarksUsingBlock:(void (^)(id <Mark> item, BOOL *stop)) block;
+
+// for the Memento pattern
+- (id)initWithCoder:(NSCoder *)coder;
+- (void)encodeWithCoder:(NSCoder *)coder;
 
 @end
